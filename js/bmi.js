ffd55540a3b2eca2c97eff4f175038046291fe1f
fly <fly@gmail.com>
2022-09-14 19:05:19 +0800

    bim更新

--- js/bmi.js
+++ js/bmi.js
@@ -15,9 +15,8 @@ calcEl.innerText = "分析";
 calcEl.addEventListener("click", () => {
 
 
-
-
     let bmiEl = document.querySelector("#comment .bmi");
+    let textEl = document.querySelector("#comment .text");
     console.log(bmiEl);
 
     let height = heightEl.value;
@@ -34,18 +33,22 @@ calcEl.addEventListener("click", () => {
     }
 
     console.log(bmi);
-    bmiEl.innerHTML = bmi;
+    bmiEl.innerHTML = ":" + bmi;
 
     let comments = [
+        { 'bmi': 45, 'color': 'red', 'comment': '你真的太胖了，請不要再吃了!' },
         { 'bmi': 35, 'color': 'red', 'comment': '異常範圍，請節食運動!' },
         { 'bmi': 24, 'color': 'green', 'comment': '正常範圍，恭喜你，請繼續保持。' },
         { 'bmi': 18.5, 'color': 'blue', 'comment': '體重過輕，請多攝取營養。' },
     ];
 
-    bmiEl.style.color = "blue";
+    bmiEl.style.color = comments[comments.length - 1]["color"];
+    textEl.innerText = comments[comments.length - 1]["comment"];
+
     for (let i = 0; i < comments.length; i++) {
         if (bmi >= comments[i].bmi) {
             bmiEl.style.color = comments[i]["color"];
+            textEl.innerText = comments[i]["comment"];
             break;
         }
     }
